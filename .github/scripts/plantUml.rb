ecb2d57e4705830454d21bd0290df27bb28561d7
Daniel Granerud <daniel.granerud@payex.com>
2020-03-10 08:21:13 +0100

    Further modification of plantUml building

--- .github/scripts/plantUml.rb
+++ .github/scripts/plantUml.rb
@@ -1,4 +1,8 @@
 require 'nokogiri'
+require 'tempfile'
+
+plant_uml_jar_file = "./.github/scripts/plantuml.1.2020.2.jar"
+plant_uml_file_name = "plant_uml.txt"
 
 html_files = Dir[ './_site/**/*.html' ].select{ |f| File.file? f } 
 
@@ -9,6 +13,10 @@ for file in html_files do
   parsed_html = Nokogiri::HTML.parse(File.open(file, "r"))
   plant_uml_tags = parsed_html.css(".language-plantUML")
   plant_uml_tags.each do |tag|
-    puts tag.text
+    # puts tag.text
+    uml_file = File.open(plant_uml_file_name, "w") { |f| f.write(tag.text)}
+    puts uml_file
+    exec "java -jar #{plant_uml_jar_file} #{plant_uml_file_name}"
+    puts uml_file
   end
 end
