ce2218bcb90a2b618fdc1104a22905c8b2e24954
Daniel Granerud <daniel.granerud@payex.com>
2020-03-11 12:59:13 +0100

    Remove debug output

--- _plugins/plant_uml.rb
+++ _plugins/plant_uml.rb
@@ -1,7 +1,6 @@
 require 'fileutils'
 
 Jekyll::Hooks.register([:pages, :posts], :post_render) do |post|
-  puts `java -version`
   plant_uml_jar_file = "./_plugins/plantuml.1.2020.2.jar"
   diagram_directory = "assets/diagrams"
 
