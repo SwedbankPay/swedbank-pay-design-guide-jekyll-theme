9814083fa21b30d81014742f4ec8cc8a76fb8054
NullableInt <daniell.granerud@payex.com>
2020-06-10 13:16:32 +0200

    DX-1031 Inspecting what I have available in the sidebar

--- _plugins/siebar.rb
+++ _plugins/siebar.rb
@@ -1,10 +1,32 @@
 require "jekyll"
+require "nokogiri"
 
 module Jekyll
-    class Sidebar < Jekyll::Generator
-
-        def generate(site)
-            
-        end
+  class Sidebar
+    def initialize      
     end
+
+    # Payload is a Jekyll::Drops::UnifiedPayloadDrop
+    def generate(payload)
+      #File.open("payload.txt", 'w') { |file| file.write(payload.page) }
+    end
+  end
+end
+
+
+Jekyll::Hooks.register :site, :post_render do |site, payload|
+  # code to call after Jekyll renders a post
+  menu = {}
+  site.pages.each.with_index do | value, index |
+    
+    doc = Nokogiri::HTML.parse(value.content)
+    doc_xpath = doc.xpath("//h[1-6]")
+    File.open("page-#{index}.txt", 'w') { |file| file.write(doc) }
+    
+  end
+  sidebar = Jekyll::Sidebar.new
+  generated = sidebar.generate payload
+  #File.open("site.txt", 'w') { |file| file.write(site.pages.methods - Object.methods) }
+
+  raise "hell"
 end
