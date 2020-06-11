5d43d7c57e7889b1267de322e846a89d3607d437
NullableInt <daniell.granerud@payex.com>
2020-06-11 13:13:32 +0200

    DX-1031 Get title and url from each file

--- _plugins/sidebar.rb
+++ _plugins/sidebar.rb
@@ -7,6 +7,17 @@ Jekyll::Hooks.register :site, :post_write do | site |
 
   Dir.glob("#{site.config["destination"]}/**/*.html") do | filename |
     puts filename
+    doc = File.open(filename) { |f| Nokogiri::HTML(f) }
+    title = doc.title
+    url = ""
+    doc.xpath("/html/head/meta").each do | meta |
+      url = meta.attr("value") if meta.attr("property").eql? "og:url"
+    end
+    
+    headers = doc.css("h1, h2, h3, h4, h5, h6")
+    headers.each do | header |
+      #puts header
+    end
   end
-
+  raise "hel"
 end
