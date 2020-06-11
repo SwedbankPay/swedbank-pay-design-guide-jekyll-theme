e8196c748de23824ffcfe206db29783e294ddc7a
NullableInt <daniell.granerud@payex.com>
2020-06-11 09:10:50 +0200

    DX-1031 Use post write hook instead to have the actually generated files

--- _plugins/sidebar.rb
+++ _plugins/sidebar.rb
@@ -1,40 +1,12 @@
 require "jekyll"
 require "nokogiri"
+require 'json'
 
-module Jekyll
-  class Sidebar
-    def initialize      
-    end
+Jekyll::Hooks.register :site, :post_write do | site |
+  File.open("site.log", 'w') { |file| file.write(site.inspect) }
 
-    # Payload is a Jekyll::Drops::UnifiedPayloadDrop
-    def generate(payload)
-      #File.open("payload.txt", 'w') { |file| file.write(payload.page) }
-    end
+  Dir.glob("#{site.config["destination"]}/**/*.html") do | filename |
+    puts filename
   end
-end
 
-
-Jekyll::Hooks.register :site, :post_render do |site, payload|
-  # code to call after Jekyll renders a post
-  menu = {}
-  site.pages.each.with_index do | page, index |
-    unless page.html?
-      next
-    end
-
-    sidebar_content = {}
-    sidebar_content["title"] = page.data["title"]
-    sidebar_content["file_name"] = page.name
-    sidebar_content["url"] = page.url
-    
-    doc = Nokogiri::HTML.parse(page.content)
-    doc_xpath = doc.css("h1, h2, h3, h4, h5, h6")
-    File.open("page-#{index}.txt", 'w') { |file| file.write("#{doc_xpath}") }
-    
-  end
-  sidebar = Jekyll::Sidebar.new
-  generated = sidebar.generate payload
-  #File.open("site.txt", 'w') { |file| file.write(site.pages.methods - Object.methods) }
-
-  raise "hell"
 end
