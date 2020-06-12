832a5ae0e830500057bab626ad00f207e9d6b2b9
NullableInt <daniell.granerud@payex.com>
2020-06-12 09:15:10 +0200

    DX-1031 Extract all headers

--- _plugins/sidebar.rb
+++ _plugins/sidebar.rb
@@ -6,18 +6,23 @@ Jekyll::Hooks.register :site, :post_write do | site |
   File.open("site.log", 'w') { |file| file.write(site.inspect) }
 
   Dir.glob("#{site.config["destination"]}/**/*.html") do | filename |
-    puts filename
+#    puts filename
     doc = File.open(filename) { |f| Nokogiri::HTML(f) }
     title = doc.title
     url = ""
     doc.xpath("/html/head/meta").each do | meta |
       url = meta.attr("value") if meta.attr("property").eql? "og:url"
     end
-    
-    headers = doc.css("h1, h2, h3, h4, h5, h6")
-    headers.each do | header |
-      #puts header
+
+
+    headers = {}
+    doc.xpath("//h2 | //h2 | //h3 | //h4 | //h5 | //h6").each do | header |
+      if not header["id"]
+        next
+      end
+      headers[header["id"]] = header.content.strip
     end
+    #puts headers
   end
   raise "hel"
 end
