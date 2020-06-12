b8ad05459f9c39c6e45e3bea53472d862f28a58b
NullableInt <daniell.granerud@payex.com>
2020-06-12 10:31:30 +0200

    Move to class

--- _plugins/sidebar.rb
+++ _plugins/sidebar.rb
@@ -1,31 +1,41 @@
 require "jekyll"
 require "nokogiri"
-require 'json'
+
+module Jekyll
+  class Sidebar
+    def post_write (site)
+      filename_with_headers = {}
+      Dir.glob("#{site.config["destination"]}/**/*.html") do | filename |
+        doc = File.open(filename) { |f| Nokogiri::HTML(f) }
+        title = doc.title
+        url = ""
+        doc.xpath("/html/head/meta").each do | meta |
+          if meta.attr("property").eql? "og:url"
+            url = meta.attr("value") 
+          end
+        end
+
+        headers = {}
+        doc.xpath("//h2 | //h2 | //h3 | //h4 | //h5 | //h6").each do | header |
+          if not header["id"]
+            next
+          end
+          child = header.last_element_child 
+          headers[header["id"]] = {
+            :content => header.content.strip,
+            :url => "#{url}#{child["href"]}"
+          }
+        end
+        filename_with_headers[filename] = headers
+      end
+      puts filename_with_headers
+      #raise "hel"
+    end
+  end
+end
 
 Jekyll::Hooks.register :site, :post_write do | site |
-  filename_with_headers = {}
-  Dir.glob("#{site.config["destination"]}/**/*.html") do | filename |
-#    puts filename
-    doc = File.open(filename) { |f| Nokogiri::HTML(f) }
-    title = doc.title
-    url = ""
-    doc.xpath("/html/head/meta").each do | meta |
-      url = meta.attr("value") if meta.attr("property").eql? "og:url"
-    end
-
-    headers = {}
-    doc.xpath("//h2 | //h2 | //h3 | //h4 | //h5 | //h6").each do | header |
-      if not header["id"]
-        next
-      end
-      child = header.last_element_child 
-      headers[header["id"]] = {
-        :content => header.content.strip,
-        :url => "#{url}#{child["href"]}"
-      }
-    end
-    filename_with_headers[filename] = headers
-  end
-  puts filename_with_headers
-  raise "hel"
+  sidebar = Jekyll::Sidebar
+  puts sidebar.inspect
+  sidebar.post_write site
 end
