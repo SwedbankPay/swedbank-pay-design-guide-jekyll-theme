f8363ed65dc1e961b4a2705688c41c7a28353dae
Phi Thien Hoang <phi-thien.hoang@payex.com>
2020-06-18 13:06:23 +0200

    Add rest of front page, tweak card styling

--- assets/js/swedbank-pay-design-guide-theme.js
+++ assets/js/swedbank-pay-design-guide-theme.js
@@ -19,57 +19,57 @@
 })();
 
 // Initialize sidebar navigation scroll activation
-(function () {
-    var headings = document.querySelectorAll("h2");
-    var tocLinks = document.querySelectorAll("nav.doc-toc ul li a");
+// (function () {
+//     var headings = document.querySelectorAll("h2");
+//     var tocLinks = document.querySelectorAll("nav.doc-toc ul li a");
 
-    var getPosition = function (el) {
-        if (el) {
-            var bodyRect = document.body.getBoundingClientRect();
-            var elemRect = el.getBoundingClientRect();
+//     var getPosition = function (el) {
+//         if (el) {
+//             var bodyRect = document.body.getBoundingClientRect();
+//             var elemRect = el.getBoundingClientRect();
 
-            return elemRect.top - bodyRect.top;
-        }
+//             return elemRect.top - bodyRect.top;
+//         }
 
-        return null;
-    };
+//         return null;
+//     };
 
-    window.addEventListener("scroll", function () {
-        var buffer = document.body.clientHeight * 0.1;
-        var currentPos = window.pageYOffset + buffer;
+//     window.addEventListener("scroll", function () {
+//         var buffer = document.body.clientHeight * 0.1;
+//         var currentPos = window.pageYOffset + buffer;
 
-        // TODO: Probably a stupid way to compute "how far left can we scroll until
-        //       we reach the bottom of the page", but it seems to work.
-        var scrollDistanceFromBottom = document.documentElement.scrollHeight
-            - document.documentElement.scrollTop
-            - document.body.clientHeight
-            - buffer;
+//         // TODO: Probably a stupid way to compute "how far left can we scroll until
+//         //       we reach the bottom of the page", but it seems to work.
+//         var scrollDistanceFromBottom = document.documentElement.scrollHeight
+//             - document.documentElement.scrollTop
+//             - document.body.clientHeight
+//             - buffer;
 
-        if (scrollDistanceFromBottom <= 0) {
-            for (var link of tocLinks) {
-                link.parentElement.classList.remove("active");
-            }
+//         if (scrollDistanceFromBottom <= 0) {
+//             for (var link of tocLinks) {
+//                 link.parentElement.classList.remove("active");
+//             }
 
-            tocLinks[tocLinks.length - 1].parentElement.classList.add("active");
-            return;
-        }
+//             tocLinks[tocLinks.length - 1].parentElement.classList.add("active");
+//             return;
+//         }
 
-        for (var i = 0; i < headings.length; i++) {
-            var heading = headings[i];
-            var headingPos = getPosition(heading);
-            var nextHeadingPos = getPosition(headings[i + 1]);
+//         for (var i = 0; i < headings.length; i++) {
+//             var heading = headings[i];
+//             var headingPos = getPosition(heading);
+//             var nextHeadingPos = getPosition(headings[i + 1]);
 
-            if (currentPos > headingPos && currentPos < nextHeadingPos) {
-                for (var link of tocLinks) {
-                    link.parentElement.classList.remove("active");
-                }
+//             if (currentPos > headingPos && currentPos < nextHeadingPos) {
+//                 for (var link of tocLinks) {
+//                     link.parentElement.classList.remove("active");
+//                 }
 
-                tocLinks[i].parentElement.classList.add("active");
-                return;
-            }
-        }
-    });
-})();
+//                 tocLinks[i].parentElement.classList.add("active");
+//                 return;
+//             }
+//         }
+//     });
+// })();
 
 // Initialize Tipue search
 (function() {
