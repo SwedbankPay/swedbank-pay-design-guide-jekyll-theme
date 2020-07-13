0ac355ac97add9aefe99a520621352c1c52e94dc
deploy.sh <asbjorn@ulsberg.no>
2020-07-14 00:31:14 +0200

    Add jekyll_asset_pipeline and move (S)CSS files into _assets

---
+++ _plugins/jekyll_asset_pipeline.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'jekyll_asset_pipeline'
+
+module JekyllAssetPipeline
+  # Converts .scss files to CSS.
+  class SassConverter < JekyllAssetPipeline::Converter
+    require 'sass'
+
+    def self.filetype
+      '.scss'
+    end
+
+    def convert
+      Sass::Engine.new(@content, syntax: :scss, load_paths: [@dirname]).render
+    end
+  end
+end
