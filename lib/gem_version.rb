fbe74d54634848457820e6d20901cd39337bdd94
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-08-21 13:53:00 +0200

    Patch gem_version.rb with version number
    Just patch gem_version.rb with version number instead of reading it from version.json

--- lib/gem_version.rb
+++ lib/gem_version.rb
@@ -1,21 +1,10 @@
 # frozen_string_literal: true
 
 module Gem
-  # The Gem specification
+  # Gem Specification
   class Specification
     def self.gem_version
-      # Required for bundle install
-      gem_version = '0.0.1.no-version'
-      version_file = File.join(__dir__, '..', 'version.json')
-
-      if File.file? version_file
-        File.open(version_file) do |file|
-          json_data = JSON.parse(file)
-          gem_version = json_data['version']
-        end
-      end
-
-      gem_version
+      '0.0.1.dev'
     end
   end
 end
