a0bfc65b3d3963defb8f36e7e22e07381462c210
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-09-19 01:32:04 +0200

    Replace string based home item spec with markup based

--- spec/sidebar_spec.rb
+++ spec/sidebar_spec.rb
@@ -28,10 +28,6 @@ describe SwedbankPay::Sidebar do
     }
 
     it {
-      is_expected.to include('<i class="material-icons">arrow_right</i><span>Home</span>')
-    }
-
-    it {
       is_expected.to include('<ul class="nav-ul"><li class="nav-leaf"><a href="/checkout/capture#step-5-capture-the-funds">Step 5: Capture the funds</a></li></ul>')
     }
 
@@ -50,7 +46,24 @@ describe SwedbankPay::Sidebar do
       end
     end
 
-    it 'has expected nav structure' do
+    it 'has checkin items' do
+      is_expected.to have_tag('li', class: 'nav-subgroup') do
+        with_tag('div', class: 'nav-subgroup-heading') do
+          with_tag('i.material-icons', text: 'arrow_right')
+          with_tag('a[href="/checkout/checkin"]', text: 'Checkin')
+        end
+        with_tag('ul', class: 'nav-ul') do
+          with_tag('li', class: 'nav-leaf') do
+            with_tag('a[href="/checkout/checkin#step-1-initiate-session-for-consumer-identification"]', text: 'Step 1: Initiate session for consumer identification')
+          end
+          with_tag('li', class: 'nav-leaf') do
+            with_tag('a[href="/checkout/checkin#step-2-display-swedbank-pay-checkin-module"]', text: 'Step 2: Display Swedbank Pay Checkin module')
+          end
+        end
+      end
+    end
+
+    it 'has introduction item' do
       is_expected.to have_tag('ul', class: 'nav-ul') do
         with_tag('li', class: 'nav-subgroup') do
           with_tag('div', class: 'nav-subgroup-heading') do
