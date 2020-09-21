db1af2097648bd6a22f3af274d58aa3aa9580142
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-09-21 12:39:44 +0200

    Replace the last string based item specs with markup based ones

--- spec/sidebar_spec.rb
+++ spec/sidebar_spec.rb
@@ -27,13 +27,22 @@ describe SwedbankPay::Sidebar do
       expect(File).to exist(index_path)
     }
 
-    it {
-      is_expected.to include('<ul class="nav-ul"><li class="nav-leaf"><a href="/checkout/capture#step-5-capture-the-funds">Step 5: Capture the funds</a></li></ul>')
-    }
+    it 'has nav leaf' do
+      is_expected.to have_tag('ul', class: 'nav-ul') do
+        with_tag('li', class: 'nav-leaf') do
+          with_tag('a[href="/checkout/capture#step-5-capture-the-funds"]', text: 'Step 5: Capture the funds')
+        end
+      end
+    end
 
-    it {
-      is_expected.to include('<i class="material-icons">arrow_right</i><a href="/checkout/payment-menu">Payment Menu</a>')
-    }
+    it 'has subgroup' do
+      is_expected.to have_tag('li', class: 'nav-subgroup') do
+        with_tag('div', class: 'nav-subgroup-heading') do
+          with_tag('i.material-icons', text: 'arrow_right')
+          with_tag('a[href="/checkout/payment-menu"]', text: 'Payment Menu')
+        end
+      end
+    end
 
     it 'has active home item' do
       is_expected.to have_tag('ul#dx-sidebar-main-nav-ul', class: 'main-nav-ul') do
