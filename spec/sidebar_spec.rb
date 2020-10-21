fc29e505c95dd915a4f23f2b61b95111c6f6ecac
deploy.sh <asbjorn@ulsberg.no>
2020-10-21 02:04:13 +0200

    publish: Made SidebarRenderer recursive
    generated from commit bd75a7d2aed71c3d1da3c9a350c88c451481d43f

--- spec/sidebar_spec.rb
+++ spec/sidebar_spec.rb
@@ -192,4 +192,31 @@ describe sidebar do
       end
     end
   end
+
+  po_path = File.join(dest_dir, 'checkout', 'features', 'payment-orders.html')
+
+  describe po_path do
+    subject { File.read(po_path) }
+
+    it {
+      expect(File).to exist(po_path)
+    }
+
+    it 'has expected nav structure' do
+      is_expected.to have_tag('ul', class: 'nav-ul') do
+        with_tag('li', class: 'nav-subgroup') do
+          with_tag('div', class: 'nav-subgroup-heading') do
+            with_tag('i.material-icons', text: 'arrow_right')
+            with_tag('a[href="/checkout/checkin"]', text: 'Checkin')
+          end
+          with_tag('ul', class: 'nav-ul') do
+            with_tag('li', class: 'nav-leaf') do
+              with_tag('a[href="/checkout/checkin#step-1-initiate-session-for-consumer-identification"]',
+                       text: 'Step 1: Initiate session for consumer identification')
+            end
+          end
+        end
+      end
+    end
+  end
 end
