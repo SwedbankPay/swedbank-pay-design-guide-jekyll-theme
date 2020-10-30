3f93bc2e78abbfdf7dca6f7015bd6636067fe162
deploy.sh <asbjorn@ulsberg.no>
2020-10-30 16:24:10 +0100

    publish: Merge 803c3e928a326fdd3d70d5fda5299c5f3b5018b5 into 307fe1a3dfef85f08d67bca622100e91236fbe02
    generated from commit e3d47c58fb215d0f6d72cdf3cd6cd28b19c595b4

--- lib/sidebar_page_title.rb
+++ lib/sidebar_page_title.rb
@@ -1,15 +1,31 @@
 # frozen_string_literal: true
 
+require 'jekyll'
+require_relative 'sidebar_page'
+
 module SwedbankPay
   # Represents the title of a SidebarPage
   class SidebarPageTitle
-    attr_reader :section, :item
+    attr_reader :main, :section
 
-    def initialize(title)
-      @title = title
-      parts = title.split('–')
-      @section = parts.first.strip
-      @item = parts.last.strip
+    def self.parse(jekyll_page, sidebar_page)
+      raise ArgumentError, 'jekyll_page cannot be nil' if jekyll_page.nil?
+      raise ArgumentError, 'jekyll_page must be a Jekyll::Page' unless jekyll_page.is_a? Jekyll::Page
+      raise ArgumentError, 'sidebar_page cannot be nil' if sidebar_page.nil?
+      raise ArgumentError, 'sidebar_page must be a SidebarPage' unless sidebar_page.is_a? SidebarPage
+
+      title = jekyll_page['title']
+      section = jekyll_page['section']
+      return nil if title.nil?
+
+      SidebarPageTitle.new(title, section, sidebar_page)
+    end
+
+    def lead
+      section = find_section(@page)
+      return section unless section.nil?
+
+      @lead
     end
 
     def to_s
@@ -24,6 +40,32 @@ module SwedbankPay
       return -1 if other.nil?
 
       @title <=> other.to_s
+    end
+
+    private
+
+    def initialize(title, section, page)
+      raise ArgumentError, 'title cannot be nil' if title.nil?
+      raise ArgumentError, 'title must be a String' unless title.is_a? String
+      raise ArgumentError, 'page cannot be nil' if page.nil?
+      raise ArgumentError, 'page must be a SidebarPage' unless page.is_a? SidebarPage
+
+      @page = page
+      @title = title
+      parts = title.split('–')
+      @section = section
+      @lead = parts.first.strip
+      @main = parts.last.strip
+    end
+
+    def find_section(page)
+      # Return the 'section' front matter if it can be found on the current page.
+      return page.title.section unless page.nil? || page.title.nil? || page.title.section.nil? || page.title.section.empty?
+
+      # Recurse upwards to the root (until there is no parent).
+      return find_section(page.parent) unless page.nil? || page.parent.nil?
+
+      nil
     end
   end
 end
--- spec/sidebar_spec.rb
+++ spec/sidebar_spec.rb
@@ -21,46 +21,95 @@ describe sidebar do
 
       its(:path) { is_expected.to eq '/' }
       its(:children) { is_expected.to be_an_instance_of SwedbankPay::SidebarPageCollection }
+      its(:hidden?) { is_expected.to be false }
 
       describe 'title' do
-        subject { sidebar.pages[0].title.to_s }
-        it { is_expected.to eq 'Home' }
+        it { expect(subject.title.to_s).to eq 'Home' }
       end
     end
 
-    describe '[1]' do
-      describe 'title' do
-        subject { sidebar.pages[1].title.to_s }
-        it { is_expected.to eq 'Page 1' }
-      end
+    describe '[1].title' do
+      it { expect(sidebar.pages[1].title.to_s).to eq 'Page 1' }
     end
 
     describe '[3]' do
       subject { sidebar.pages[3] }
 
       its(:path) { is_expected.to eq '/checkout/' }
-
-      describe '#active?' do
-        it { expect(subject.active?('/checkout/after-payment')).to be true }
-      end
+      its(:active?, '/checkout/') { is_expected.to be true }
+      its(:active?, '/checkout/after-payment') { is_expected.to be true }
+      its(:active?, '/checkout/features/payment-orders') { is_expected.to be true }
+      its(:active?, '/checkout/features/payment-orders', is_leaf: true) { is_expected.to be false }
+      its(:hidden?) { is_expected.to be false }
 
       describe 'children[3]' do
         subject { sidebar.pages[3].children[3] }
 
         its(:path) { is_expected.to eq '/checkout/after-payment' }
+        its(:active?, '/checkout/after-payment') { is_expected.to be true }
+      end
 
-        describe '#active?' do
-          it { expect(subject.active?('/checkout/after-payment')).to be true }
+      describe 'children[4]' do
+        subject { sidebar.pages[3].children[4] }
+
+        its(:path) { is_expected.to eq '/checkout/features/' }
+        its(:active?, '/checkout/features/payment-orders') { is_expected.to be true }
+        its(:active?, '/checkout/features/payment-orders', is_leaf: true) { is_expected.to be false }
+
+        describe 'children[0]' do
+          subject { sidebar.pages[3].children[4].children[0] }
+
+          its(:path) { is_expected.to eq '/checkout/features/payment-orders' }
+          its(:active?, '/checkout/features/payment-orders') { is_expected.to be true }
         end
       end
     end
 
     describe '[5]' do
+      subject { sidebar.pages[5] }
+
+      describe 'title' do
+        it { expect(subject.title.to_s).to eq 'Payments' }
+      end
+
+      describe 'children' do
+        subject { sidebar.pages[5].children }
+
+        its(:length) { is_expected.to eq 3 }
+
+        describe '[2]' do
+          subject { sidebar.pages[5].children[2] }
+
+          its(:hidden?) { is_expected.to be true }
+
+          describe 'title' do
+            it { expect(subject.title.to_s).to eq 'Secret payments' }
+          end
+
+          describe 'children' do
+            subject { sidebar.pages[5].children[2].children }
+
+            its(:length) { is_expected.to eq 1 }
+
+            describe '[0]' do
+              subject { sidebar.pages[5].children[2].children[0] }
+
+              its(:hidden?) { is_expected.to be true }
+
+              describe 'title' do
+                it { expect(subject.title.to_s).to eq 'Secrets in payments' }
+              end
+            end
+          end
+        end
+      end
+    end
+
+    describe '[6]' do
       subject { sidebar.pages[6] }
 
       describe 'title' do
-        subject { sidebar.pages[6].title.to_s }
-        it { is_expected.to eq 'Resources' }
+        it { expect(subject.title.to_s).to eq 'Resources' }
       end
 
       describe 'children' do
@@ -68,27 +117,16 @@ describe sidebar do
 
         its(:length) { is_expected.to eq 6 }
 
-        describe '[0]' do
-          subject { sidebar.pages[6].children[0] }
-
-          describe 'title' do
-            subject { sidebar.pages[6].children[0].title.to_s }
-            it { is_expected.to eq 'Alpha' }
-          end
+        describe '[0].title' do
+          it { expect(subject[0].title.to_s).to eq 'Alpha' }
         end
 
-        describe '[1]' do
-          describe 'title' do
-            subject { sidebar.pages[6].children[1].title.to_s }
-            it { is_expected.to eq 'Beta' }
-          end
+        describe '[1].title' do
+          it { expect(subject[1].title.to_s).to eq 'Beta' }
         end
 
-        describe '[1]' do
-          describe 'title' do
-            subject { sidebar.pages[6].children[2].title.to_s }
-            it { is_expected.to eq 'Gamma' }
-          end
+        describe '[2].title' do
+          it { expect(subject[2].title.to_s).to eq 'Gamma' }
         end
       end
     end
