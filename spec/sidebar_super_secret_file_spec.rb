47b422db80fbb444b1a65c65431633b5ac3bde94
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-10-30 15:54:11 +0100

    Complete the super secret spec and remove leftover comment

--- spec/sidebar_super_secret_file_spec.rb
+++ spec/sidebar_super_secret_file_spec.rb
@@ -14,41 +14,40 @@ describe SwedbankPay::Sidebar do
       expect(File).to exist(super_secret_path)
     }
 
-    # <li class="nav-subgroup active">
-    #   <div class="nav-subgroup-heading">
-    #     <i class="material-icons">arrow_right</i>
-    #     <a href="/payments/secrets/">Secret payments</a>
-    #   </div>
-    #   <ul class="nav-ul">
-    #     <li class="nav-subgroup">
-    #       <div class="nav-subgroup-heading">
-    #         <i class="material-icons">arrow_right</i>
-    #         <a href="/payments/secrets/">Secret payments</a>
-    #       </div>
-    #       <ul class="nav-ul">
-    #         <li class="nav-leaf"><a href="/payments/secrets/#how-we-do-secret-payments">How we do secret payments</a></li>
-    #       </ul>
-    #     </li>
-    #     <li class="nav-subgroup active">
-    #       <div class="nav-subgroup-heading">
-    #         <i class="material-icons">arrow_right</i>
-    #         <a href="/payments/secrets/super-secret">Secrets in payments</a>
-    #       </div>
-    #       <ul class="nav-ul">
-    #         <li class="nav-leaf"><a href="/payments/secrets/super-secret#dont-render-this">Don’t render this</a></li>
-    #       </ul>
-    #     </li>
-    #   </ul>
-    # </li>
-
     # The navigation for a section that has `hide_from_sidebar: true` should not
     # be hidden from itself.
     it 'has expected nav structure' do
-      is_expected.to have_tag('ul', class: 'nav-ul') do
-        with_tag('li', class: 'nav-subgroup active') do
-          with_tag('div', class: 'nav-subgroup-heading') do
+      is_expected.to have_tag('ul.nav-ul') do
+        with_tag('li.nav-subgroup.active') do
+          with_tag('div.nav-subgroup-heading') do
             with_tag('i.material-icons', text: 'arrow_right')
             with_tag('a[href="/payments/secrets/"]', text: 'Secret payments')
+          end
+
+          with_tag('ul.nav-ul') do
+            with_tag('li.nav-subgroup') do
+              with_tag('div.nav-subgroup-heading') do
+                with_tag('i.material-icons', text: 'arrow_right')
+                with_tag('a[href="/payments/secrets/"]', text: 'Secret payments')
+              end
+              with_tag('ul.nav-ul') do
+                with_tag('li.nav-leaf') do
+                  with_tag('a[href="/payments/secrets/#how-we-do-secret-payments"]', text: 'How we do secret payments')
+                end
+              end
+            end
+
+            with_tag('li.nav-subgroup.active') do
+              with_tag('div.nav-subgroup-heading') do
+                with_tag('i.material-icons', text: 'arrow_right')
+                with_tag('a[href="/payments/secrets/super-secret"]', text: 'Secrets in payments')
+              end
+              with_tag('ul.nav-ul') do
+                with_tag('li.nav-leaf') do
+                  with_tag('a[href="/payments/secrets/super-secret#dont-render-this"]', text: 'Don’t render this')
+                end
+              end
+            end
           end
         end
       end
