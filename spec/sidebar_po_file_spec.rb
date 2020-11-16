a8d81419fdb87aebe0e735dc2389db66179eb610
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-11-16 15:32:30 +0100

    DX-1288: Add specs for the page title

--- spec/sidebar_po_file_spec.rb
+++ spec/sidebar_po_file_spec.rb
@@ -33,13 +33,17 @@ describe SwedbankPay::Sidebar do
       end
     end
 
-    it 'has expected titles' do
+    it 'has expected title header' do
       is_expected.to have_tag('div.title-header') do
         with_tag('div.title-header-container') do
           with_tag('h4', text: 'Checkout Features')
           with_tag('h1', text: 'Payment Orders')
         end
       end
+    end
+
+    it 'has expected page title' do
+      is_expected.to have_tag('title', text: 'Checkout Features – Payment Orders')
     end
   end
 end
