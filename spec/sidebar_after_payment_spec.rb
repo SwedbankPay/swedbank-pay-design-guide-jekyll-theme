72450d0c432557b44ba24f4bacdd7ad26a4a37ad
trieutran91 <trieu.tran@payex.com>
2023-01-24 13:30:02 +0100

    Fixed core_feature_spec

--- spec/sidebar_after_payment_spec.rb
+++ spec/sidebar_after_payment_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'sidebar'
+require "sidebar"
 
 describe SwedbankPay::Sidebar do
-  include_context 'shared'
-  after_payment_path = File.expand_path(File.join(@dest_dir, 'checkout', 'v2', 'after-payment.html'))
+  include_context "shared"
+  after_payment_path = File.expand_path(File.join(@dest_dir, "checkout", "v2", "after-payment.html"))
 
   describe after_payment_path do
     subject { File.read(after_payment_path) }
@@ -13,19 +13,19 @@ describe SwedbankPay::Sidebar do
       expect(File).to exist(after_payment_path)
     }
 
-    it 'has active item' do
-      is_expected.to have_tag('#dx-sidebar-main-nav-ul') do
-        with_tag('li.group.active') do
-          with_tag('a[href="/checkout/v2/after-payment"]', text: 'After Payment') do
-            with_tag('i.material-icons-outlined')
+    it "has active item" do
+      is_expected.to have_tag("#dx-sidebar-main-nav-ul") do
+        with_tag("li.group.active") do
+          with_tag('a[href="/checkout/v2/after-payment"]', text: "After Payment") do
+            with_tag("i.material-icons-outlined")
           end
         end
-        with_tag('li.main-nav-li.active') do
-          with_tag('nav.sidebar-secondary-nav') do
-            with_tag('ul.secondary-nav-ul') do
-              with_tag('li.secondary-nav-li.leaf.active') do
-                with_tag('a[href="/checkout/v2/"]', text: 'Checkout v2') do
-                  with_tag('i.material-icons-outlined')
+        with_tag("li.main-nav-li.active") do
+          with_tag("nav.sidebar-secondary-nav") do
+            with_tag("ul.secondary-nav-ul") do
+              with_tag("li.secondary-nav-li.leaf.active") do
+                with_tag('a[href="/checkout/v2/"]', text: "Checkout v2") do
+                  with_tag("i.material-icons-outlined")
                 end
               end
             end
@@ -34,8 +34,8 @@ describe SwedbankPay::Sidebar do
       end
     end
 
-    it 'has expected page title' do
-      is_expected.to have_tag('title', text: 'Checkout v2 – After Payment')
+    it "has expected page title" do
+      is_expected.to have_tag("title", text: "Checkout v2 – After Payment")
     end
   end
 end
--- spec/sidebar_checkout_core_features_file_spec.rb
+++ spec/sidebar_checkout_core_features_file_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'sidebar'
+require "sidebar"
 
 describe SwedbankPay::Sidebar do
-  include_context 'shared'
-  checkout_core_features_path = File.join(@dest_dir, 'checkout', 'v2', 'features', 'core', 'index.html')
+  include_context "shared"
+  checkout_core_features_path = File.join(@dest_dir, "checkout", "v2", "features", "core", "index.html")
 
   describe checkout_core_features_path do
     subject { File.read(checkout_core_features_path) }
@@ -13,29 +13,23 @@ describe SwedbankPay::Sidebar do
       expect(File).to exist(checkout_core_features_path)
     }
 
-    it 'is missing header navigation' do
-      is_expected.to have_tag('li.main-nav-li.active') do
-        with_tag('li.main-nav-li.active') do
+    it "is missing header navigation" do
+      is_expected.to have_tag("li.main-nav-li.active") do
+        with_tag("li.main-nav-li.active") do
           with_tag('a[href="/checkout/"]')
-          with_tag('nav.sidebar-secondary-nav') do
-              with_tag('header.secondary-nav-header', Text: 'Checkout') do
-                with_tag('ul.secondary-nav-ul') do
-                  with_tag('ul.nav-ul') do
-                    with_tag('li.nav-subgroup', with: { class: 'active' }) do
-                      with_tag('ul.nav-ul') do
-                        with_tag('li.nav-subgroup', with: { class: 'active' }) do
-                          with_tag('a[href="/checkout/v2/features/"]', text: 'Features') do
-                            with_tag('li', with: { class: 'leaf.active' }) do
-                              with_tag('a[href="/checkout/v2/features/core/"]', text: 'Core Features')
-                            end
-                          end
-                        end
-                      end
-                    end
+          with_tag("nav.sidebar-secondary-nav") do
+            with_tag("header.secondary-nav-header", Text: "Checkout")
+            with_tag("ul.secondary-nav-ul") do
+              with_tag("li.leaf", with: { class: "active" }) do
+                with_tag('a[href="/checkout/v2/features/"]', text: "Features")
+                with_tag("ul") do
+                  with_tag("li.leaf", with: { class: "active" }) do
+                    with_tag('a[href="/checkout/v2/features/core/"]', text: "Core Features")
                   end
                 end
               end
             end
+          end
         end
       end
     end
